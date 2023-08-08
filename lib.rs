028ac6a24a6418611c70108956e92e9bfee8bb63
Schmidt Cristian Hernán <schcriher@gmail.com>
2023-08-08 00:26:59 -0300

    clippy lints recommendation
    https://rust-lang.github.io/rust-clippy/master/index.html#/bool_assert_comparison

--- lib.rs
+++ lib.rs
@@ -54,16 +54,16 @@ mod flipper {
         #[ink::test]
         fn default_works() {
             let flipper = Flipper::default();
-            assert_eq!(flipper.get(), false);
+            assert!(!flipper.get());
         }
 
         /// We test a simple use case of our contract.
         #[ink::test]
         fn it_works() {
             let mut flipper = Flipper::new(false);
-            assert_eq!(flipper.get(), false);
+            assert!(!flipper.get());
             flipper.flip();
-            assert_eq!(flipper.get(), true);
+            assert!(flipper.get());
         }
     }
 
